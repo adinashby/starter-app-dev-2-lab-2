bbca8b9a4ebd0a08168c0770f9acd3ebdfc9475e
Adin Ashby <adin.ashby@gmail.com>
2022-10-07 08:25:07 -0400

    first commit

---
+++ bin/LabTwo.dart
@@ -0,0 +1,15 @@
+/**
+ * Write your test code below in the main (optional).
+ *
+ * @author adinashby
+ */
+
+void main(List<String> arguments) {}
+
+/**
+ * Please refer to the README file for question(s) description
+ */
+
+bool isValidSudoku(List<List<String>> board) {
+  return false;
+}
